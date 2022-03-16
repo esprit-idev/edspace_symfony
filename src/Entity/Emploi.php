79f566ea7e383af00cb549adbe70fe3628080c2c
Eslem <sousououederni@gmail.com>
2022-03-16 12:19:56 +0100

    added json methods to publicationnews and emploi and the different categories

--- src/Entity/Emploi.php
+++ src/Entity/Emploi.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\EmploiRepository;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=EmploiRepository::class)
@@ -15,23 +16,27 @@ class Emploi
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups("emplois")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank(message="le titre est requi")
+     * @Groups("emplois")
      */
     private $title;
 
     /**
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank(message="ajouter une discription")
+     * @Groups("emplois")
      */
     private $content;
 
     /**
      * @ORM\Column(type="date")
+     * @Groups("emplois")
      */
     private $date;
 
@@ -39,12 +44,14 @@ class Emploi
      * @ORM\ManyToOne(targetEntity=CategorieEmploi::class, inversedBy="emplois")
      * @ORM\JoinColumn(nullable=true, onDelete="CASCADE")
      * @Assert\NotBlank(message="add category !")
+     * @Groups("emplois")
      */
     private $categoryName;
 
     /**
      * @ORM\OneToOne(targetEntity=Image::class, cascade={"persist", "remove"})
      * @Assert\NotBlank(message="Ajouter une Image")
+     * @Groups("emploiimg")
      */
     private $image;
 
