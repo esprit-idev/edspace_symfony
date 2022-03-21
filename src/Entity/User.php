fab58bd2693fc8b24da146606d936afb491e38a4
AnasHouissa <AnasHouissa@gmail.com>
2022-03-21 15:42:01 +0100

    json clubs front office

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
