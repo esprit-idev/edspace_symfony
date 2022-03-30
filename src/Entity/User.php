0e1b86c8651153deee4c41b3761459c566ba9031
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2022-03-30 18:36:24 +0100

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,8 +5,6 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Symfony\Component\Serializer\Annotation\Groups;
-
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
